a91058996a4450ea2ad3200fd7e588139d951374
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2026-07-20 16:12:38 -0300

    fix: import

--- app/modules/anilist/anime_details/service.py
+++ app/modules/anilist/anime_details/service.py
@@ -4,7 +4,10 @@ from app.modules.anilist.anime_details.dto.anime_progress_dto import (
     AnimeProgressDTO,
     FuzzyDateDTO,
 )
-from app.modules.anilist.anime_details.dto.anime_resource import AnimeDetailsDTO
+from app.modules.anilist.anime_details.dto.anime_resource import (
+    AnimeDetailsDTO,
+    AnimeResource,
+)
 from app.modules.anilist.anime_details.queries import (
     ANIME_DETAILS,
     SAVE_ANIME_PROGRESS,
