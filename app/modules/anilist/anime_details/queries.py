cdf311e2a5353a6b7c4c89c4e0736332047ea246
Jacson Sávio de Souza <jacsonsavio7@gmail.com>
2026-07-30 16:04:36 -0300

    fix: correct update episode progress query

--- app/modules/anilist/anime_details/queries.py
+++ app/modules/anilist/anime_details/queries.py
@@ -130,7 +130,7 @@ mutation (
 """
 
 UPDATE_EPISODE_PROGRESS = """
-    mutation($mediaId, $progress) {
+    mutation($mediaId: Int, $progress: Int) {
         SaveMediaListEntry(
             mediaId: $mediaId,
             progress: $progress
